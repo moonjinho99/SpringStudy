d1cd99c545dc7b69d816cc3379c38494ea3273bd
moonjinho99 <anszl789@naver.com>
2025-02-07 16:09:17 +0900

    네이버 로그인 테스트

---
+++ src/main/java/com/example/nloginTest/NloginTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.nloginTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NloginTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NloginTestApplication.class, args);
+	}
+	
+}
